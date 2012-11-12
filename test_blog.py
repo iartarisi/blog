dca287a36fde6bd15de888b068aad55923c6e39d
Ionuț Arțăriși <mapleoin@lavabit.com>
2012-11-12 23:11:50 +0100

    add gplv3 license

--- test_blog.py
+++ test_blog.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2012 Ionuț Arțăriși <mapleoin@lavabit.com>
+# This file is part of pyblee.
+
+# pyblee is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pyblee is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pyblee.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import os
